6d2053fdefa0dd430ba100d4a5d7ed1596ee0aa0
ux-ui-pro <maxim@ux-ui.pro>
2023-09-16 00:28:26 +0300

    issue: removed unnecessary checks

--- src/index.js
+++ src/index.js
@@ -1,77 +1,48 @@
 class MasonrySimple {
   constructor() {
-    this.grid = null;
+    this.container = null;
     this.gridItems = [];
-    this.resizeObserver = null;
     this.rowHeight = 1;
-    this.requestAnimationFrameId = null;
-    this.pendingResize = false;
-  }
-
-  resizeItem(item) {
-    const rowSpan = Math.ceil(
-      (item.clientHeight + this.rowGap) / (this.rowHeight + this.rowGap),
-    );
-
-    const newItem = item;
-
-    newItem.style.gridRowEnd = `span ${rowSpan}`;
+    this.rowGap = 0;
+    this.resizeObserver = new ResizeObserver(this.resizeAllItems.bind(this));
   }
 
   resizeAllItems() {
-    if (this.pendingResize) return;
+    this.container.style.alignItems = 'start';
+    this.gridItems.forEach((item) => {
+      const rowSpan = Math.ceil(
+        (item.clientHeight + this.rowGap) / (this.rowHeight + this.rowGap),
+      );
 
-    this.pendingResize = true;
+      const newItem = item;
 
-    if (!this.requestAnimationFrameId) {
-      this.requestAnimationFrameId = requestAnimationFrame(() => {
-        this.grid.style.alignItems = 'start';
-        this.gridItems.forEach((item) => this.resizeItem(item));
-        this.pendingResize = false;
-        this.requestAnimationFrameId = null;
-      });
-    }
+      newItem.style.gridRowEnd = `span ${rowSpan}`;
+    });
   }
 
   static init(options = {}) {
     const { container = '.masonry' } = options;
     const masonry = new MasonrySimple();
 
-    masonry.grid = container instanceof HTMLElement
+    masonry.container = container instanceof HTMLElement
       ? container
       : document.querySelector(container);
 
-    if (!masonry.grid) return;
+    if (!masonry.container) return;
 
-    masonry.gridItems = masonry.grid.children.length
-      ? Array.from(masonry.grid.children)
-      : [];
-
-    masonry.grid.style.contain = 'layout';
-
-    masonry.rowGap = parseInt(
-      window.getComputedStyle(masonry.grid).getPropertyValue('grid-row-gap'),
-      10,
-    );
-
-    masonry.resizeObserver = new ResizeObserver(
-      masonry.resizeAllItems.bind(masonry),
-    );
-
-    masonry.resizeObserver.observe(masonry.grid);
+    masonry.gridItems = Array.from(masonry.container.children);
+    masonry.container.style.contain = 'layout';
+    masonry.rowGap = parseInt(getComputedStyle(masonry.container).rowGap, 10);
+    masonry.resizeObserver.observe(masonry.container);
     masonry.resizeAllItems();
 
     return masonry;
   }
 
   destroy() {
-    if (this.resizeObserver) {
-      this.resizeObserver.unobserve(this.grid);
-      this.resizeObserver = null;
-    }
-
-    this.grid.style.contain = '';
-    this.grid.style.alignItems = '';
+    this.resizeObserver.unobserve(this.container);
+    this.container.style.contain = '';
+    this.container.style.alignItems = '';
     this.gridItems.forEach((item) => {
       const newItem = item;
 
